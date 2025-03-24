be898d7562601d20f426bc66c77af54824954de6
Bansil <bansilmarsoniya@gmail.com>
2025-03-24 18:48:52 +0530

    Making New File 01_Problem.c

--- CH - 01/PracticeSet/01_Problem1.c
+++ CH - 01/PracticeSet/01_Problem1.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
-int main(){
-    
+int main()
+{
+    // int length = 2;
+    // int breadth = 4;
+    int length, breadth;
+    printf("Enter Length\n");
+    scanf("%d", &length);
+
+    printf("Enter breadth\n");
+    scanf("%d", &breadth);
+
+    printf("Your Area Of Rectangle is %d", length * breadth);
     return 0;
 }
