199d2198d42219f1ef1bcbf4a084dbc6fc147100
Bansil <bansilmarsoniya@gmail.com>
2025-03-25 18:16:35 +0530

    Making New File 04_Problem.c

---
+++ CH - 01/PracticeSet/04_problem.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    float p = 34;      
+    int r = 8;      
+    int t = 5;      
+    printf("The value of Simple Interest is %f", (p * r * t)/100);
+    return 0;
+}
