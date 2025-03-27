130471f7f5bc3ad1ba90c147e3080f5e8dd11e57
Bansil <bansilmarsoniya@gmail.com>
2025-03-27 23:24:31 +0530

    New File 01_variable.c

--- CH - 02/01_variables.c
+++ CH - 02/01_variables.c
@@ -3,6 +3,8 @@
 int main(){
     int i = 10;
     int j = i;
-    //%d is called
+    // %d is called formate specifier
+    // %d is for "int", %f is for "float" AND %c is for "char"
+    printf("The value of i is %d\n and value of j is %d", i, j );
     return 0;
 }
