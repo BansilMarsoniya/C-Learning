ceea514c539a7f584f5b45db16e6f567b01ac155
Bansil <bansilmarsoniya@gmail.com>
2025-03-27 14:12:33 +0530

    Start New Chapter

--- CH - 01/07_escape_sequence.c
+++ CH - 01/07_escape_sequence.c
@@ -6,7 +6,7 @@ int main()
     char ch = 'a';
     ch = 'c';
     ch = '/n';                        // Represent a New line
-    printf("Hey I'm Good\"\n  Nice"); // \" is present Double quote ("...") & We can also use Single quote('...').
+    printf("Hey I'm Good\"\n  Nice"); // (\") is present Double quote ("...") & We can also use Single quote('...').
 
     return 0;
 }
--- CH - 02/01_variables.c
+++ CH - 02/01_variables.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main(){
-    
+    int i = 10;
+    int j = i;
+    //%d is called
     return 0;
 }
