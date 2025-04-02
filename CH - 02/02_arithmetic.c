ed7c784e903702ba96021dba06f9fa3f0cfa7eb7
Bansil <bansilmarsoniya@gmail.com>
2025-04-02 14:17:47 +0530

    solve spelling mistake in 02_arithmetic.c

--- CH - 02/02_arithmetic.c
+++ CH - 02/02_arithmetic.c
@@ -4,7 +4,7 @@ int main(){
     int a = 4;
     int b = 3;
     int c = a + b;
-    printf("The value of a i %d and value of b is %d and sum is %d", a,b,c);
+    printf("The value of a is %d and value of b is %d and sum is %d", a,b,c);
     //modulus operator is used to get the "remainder"
     printf("\nThe remainder when a is divided by b is %d", a%b);
     return 0;
